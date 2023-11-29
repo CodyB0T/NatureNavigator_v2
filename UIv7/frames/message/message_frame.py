48691d049f920054d314c4bf3707e2f5bb569239
CodyB0T <59010772+CodyB0T@users.noreply.github.com>
2023-11-29 13:31:56 -0500

    new changes

--- UIv7/frames/message/message_frame.py
+++ UIv7/frames/message/message_frame.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+
 from tkinter import Canvas, Button, PhotoImage
+
 from PIL import Image, ImageTk
+
 import threading
+
 import pandas as pd
 
 
@@ -22,21 +26,29 @@ class Message(tk.Frame):
             highlightthickness=0,
             relief="ridge",
         )
+
         self.canvas1.place(x=0, y=0)
 
         # ====================================================================================================================
+
         # Background Image of Hiker Lost
 
         self.original_image = Image.open(
             "frames/message/assets/Hiker_Texting_Resized.png"
         )
+
         self.resized_image = self.original_image.resize((1200, 800))
+
         self.tk_image = ImageTk.PhotoImage(self.resized_image)
+
         self.canvas1.create_image(0, 0, image=self.tk_image, anchor="nw")
 
         # ====================================================================================================================
+
         # Canvas for Banner1 (Communication Services)
+
         #
+
         self.canvas2 = Canvas(
             self,
             bg="#006400",
@@ -60,8 +72,11 @@ class Message(tk.Frame):
         )
 
         # ==========================================================================================================================
+
         # Canvas for Banner2, with Delta Logo
+
         #
+
         self.canvas3 = Canvas(
             self,
             bg="#006400",
@@ -74,8 +89,11 @@ class Message(tk.Frame):
         self.canvas3.place(x=1015, y=30)
 
         self.original_image4 = Image.open("frames/message/assets/delta_logo.png")
+
         self.resized_image = self.original_image4.resize((60, 60))
+
         self.tk_image4 = ImageTk.PhotoImage(self.resized_image)
+
         self.canvas3.create_image(105, 0.5, image=self.tk_image4, anchor="nw")
 
         self.tempText = self.canvas3.create_text(
@@ -88,11 +106,13 @@ class Message(tk.Frame):
         )
 
         # ====================================================================================================================
+
         # Message Canvas
 
         self.messageString = ""
 
         # messagecanvas
+
         self.messagesCanvas = tk.Canvas(
             self,
             bg="#006400",
@@ -103,7 +123,9 @@ class Message(tk.Frame):
             highlightthickness=3,
             relief="ridge",
         )
+
         self.messagesCanvas.place(x=50, y=150)
+
         # self.messagesCanvas.place(x=self.findCenterx(self.messagesCanvas), y=50)
 
         self.messages = self.messagesCanvas.create_text(
@@ -115,10 +137,13 @@ class Message(tk.Frame):
         )
 
         # ============================================================================================================================
+
         # Buttons with predefined messages on them
+
         # Keep this as self reference: self.helloButton.place(x=self.findCenterx(self.helloButton), y=400)
 
         # Hello
+
         self.helloButton = tk.Button(
             self,
             text="Hello",
@@ -126,9 +151,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.helloButton.place(x=25, y=500)
 
         # Hi there
+
         self.hi_there_Button = tk.Button(
             self,
             text="Hi there!!!",
@@ -136,9 +163,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.hi_there_Button.place(x=25, y=600)
 
         # Goodbye
+
         self.Goodbye_Button = tk.Button(
             self,
             text="Goodbye",
@@ -146,9 +175,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.Goodbye_Button.place(x=25, y=700)
 
         # Yes
+
         self.yes_Button = tk.Button(
             self,
             text="Yes",
@@ -156,9 +187,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.yes_Button.place(x=225, y=500)
 
         # No
+
         self.no_Button = tk.Button(
             self,
             text="No",
@@ -166,9 +199,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.no_Button.place(x=225, y=600)
 
         # Maybe
+
         self.maybe_Button = tk.Button(
             self,
             text="Maybe",
@@ -176,9 +211,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.maybe_Button.place(x=225, y=700)
 
         # Are you ok?
+
         self.are_you_ok_Button = tk.Button(
             self,
             text="Are you ok?",
@@ -186,9 +223,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.are_you_ok_Button.place(x=425, y=500)
 
         # Where are you?
+
         self.where_are_you_Button = tk.Button(
             self,
             text="Where are you?",
@@ -196,9 +235,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.where_are_you_Button.place(x=425, y=600)
 
         # Quick check-in!
+
         self.check_in_Button = tk.Button(
             self,
             text="Quick check-in!",
@@ -206,9 +247,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.check_in_Button.place(x=425, y=700)
 
         # Location update, please!
+
         self.location_please_Button = tk.Button(
             self,
             text="Location update, please! ",
@@ -216,9 +259,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.location_please_Button.place(x=625, y=500)
 
         # Send GPS coordinates
+
         self.GPS_Coordinates_Button = tk.Button(
             self,
             text="[Send GPS Coordinates]",
@@ -226,9 +271,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.GPS_Coordinates_Button.place(x=625, y=600)
 
         # Water that's blessed by the gods themselves
+
         self.water_Button = tk.Button(
             self,
             text="Found safe drinking water",
@@ -238,9 +285,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.water_Button.place(x=625, y=700)
 
         # Wanna meet up?
+
         self.wanna_meet_up_Button = tk.Button(
             self,
             text="Wanna meet up?",
@@ -248,9 +297,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.wanna_meet_up_Button.place(x=825, y=500)
 
         # How's your hike going?
+
         self.hike_going_Button = tk.Button(
             self,
             text="How's your hike going?",
@@ -258,9 +309,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.hike_going_Button.place(x=825, y=600)
 
         # Are you close?
+
         self.are_you_close_Button = tk.Button(
             self,
             text="Are you close?",
@@ -268,9 +321,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.are_you_close_Button.place(x=825, y=700)
 
         # Trail conditions?
+
         self.trail_Button = tk.Button(
             self,
             text="Trail conditions?",
@@ -278,9 +333,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.trail_Button.place(x=1025, y=500)
 
         # Safe
+
         self.safe_Button = tk.Button(
             self,
             text="safe",
@@ -288,9 +345,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.safe_Button.place(x=1025, y=600)
 
         # unsafe
+
         self.unsafe_Button = tk.Button(
             self,
             text="unsafe",
@@ -298,9 +357,11 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.unsafe_Button.place(x=1025, y=700)
 
         # Danger! Stay away!
+
         self.danger_Button = tk.Button(
             self,
             text="Danger! Stay away!",
@@ -308,48 +369,64 @@ class Message(tk.Frame):
             width=20,
             height=3,
         )
+
         self.danger_Button.place(x=825, y=260)
 
         # Add a new button to clear the canvas
+
         self.clearButton = tk.Button(
             self,
             text="[Update Messages]",
-            command=self.clearMessages,
+            command=self.updateMessageLora,
             width=20,
             height=3,
         )
+
         self.clearButton.place(x=1025, y=260)
 
     # ============================================================================================================================
 
     # Function to clear messages
+
     def updateMessageLora(self):
         self.Lora.add_to_queue(self.Lora.active_listen)
-        self.updateMessageBorad()
+
+        self.updateMessageBoard()
 
     # update message window of the id loops every 5 sec
+
     def updateMessageBoard(self):
+        print(self.Lora.messageDone)
+
         if self.Lora.messageDone == True:
             self.Lora.messageDone = False
 
             id = "1111111111111111"
 
             df = pd.read_csv("data/messages.csv")
+
             clean = df.dropna(subset=[id])[id]
+
             self.messageString = ""
+
             for x in clean:
                 self.messageString = self.messageString + "\n" + x
 
+            print("message updated")
+
             self.messagesCanvas.itemconfig(self.messages, text=self.messageString)
+
         else:
             self.after(1000, self.updateMessageBoard)
 
     def clearMessages(self):
         self.messageString = ""
+
         self.messagesCanvas.itemconfig(self.messages, text=self.messageString)
 
     def addMessage(self, s):
         self.messageString = self.messageString + "\n" + s
+
         self.messagesCanvas.itemconfig(self.messages, text=self.messageString)
 
         # self.threadMessage(s)
@@ -361,17 +438,23 @@ class Message(tk.Frame):
     def threadMessage(self, s):
         if self.thread and self.thread.is_alive():
             print("Threaded function is already running.")
+
         else:
             # Create and start a new thread
+
             self.thread = threading.Thread(
                 target=self.Lora.send_text, args=("b69b9d14e5e5b0c0", f"{s}")
             )
+
             self.thread.start()
+
             print("Threaded function started.")
 
     def findCenterx(self, object):
         self.object = object
+
         self.width = 1200
+
         self.objectWidth = object.winfo_reqwidth()
 
         self.x = (self.width - self.objectWidth) // 2
@@ -381,10 +464,13 @@ class Message(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
+
     root.geometry("1200x800")
 
     frame = Message(root)
+
     frame.config(width=1200, height=800)
+
     frame.pack()
 
     root.mainloop()
--- UIv7/sensorLibs/Lora.py
+++ UIv7/sensorLibs/Lora.py
@@ -1,10 +1,17 @@
 import pandas as pd
+
 import serial
+
 import json
+
 from datetime import datetime, timedelta
+
 import os
+
 import time
+
 import queue
+
 import threading
 
 
@@ -15,85 +22,121 @@ class Lora:
         # Constants for the serial connection
 
         # know when to update page
+
         self.messageDone = False
 
         SERIAL_PORT = "/dev/ttyUSB0"
+
         BAUD_RATE = 115200
 
         # Initialize serial connection to the Arduino
+
         self.ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+
         self.initialize_LoRaWAN()
 
         # Create a queue for method calls
+
         self.method_queue = queue.Queue()
 
         # Create a thread to process the method queue
+
         self.thread = threading.Thread(target=self.process_method_queue, daemon=True)
+
         self.thread.start()
 
     def add_to_queue(self, method, *args, **kwargs):
         """Add a method and its arguments to the queue."""
+
         self.method_queue.put((method, args, kwargs))
 
     def process_method_queue(self):
         """Process methods in the queue."""
+
         while True:
             try:
                 method, args, kwargs = self.method_queue.get()
+
                 print(self.method_queue.qsize())
+
                 method(*args, **kwargs)
+
             except queue.Empty:
                 pass  # Ignore empty queue
 
     def initialize_LoRaWAN(self, retry_limit=3):
         """Attempt to initialize LoRaWAN connection with retry mechanism."""
+
         time.sleep(4)  # Delay for Arduino boot up
+
         self.ser.write(b"#BEGIN\n")
+
         for attempt in range(retry_limit):
             bol = self.ser.readline()
+
             if bol.decode().strip() == "#JOINED":
                 self.connected_bool = True
+
                 self.ser.write(b"#BLANK\n")
+
                 time.sleep(10)
+
                 return True
 
             time.sleep(5)  # Delay between retries
 
     def terminate_LoRaWAN(self):
         """Terminate the LoRaWAN connection."""
+
         self.ser.write(b"#QUIT\n")
+
         self.connected_bool = False
 
     def change_system_time(self, time_value):
         date_parts = time_value.split(", ")
+
         formatted_date = datetime.strptime(date_parts[0], "%m/%d/%Y").strftime(
             "%Y-%m-%d"
         )
+
         formatted_time = date_parts[1]
+
         formatted_system_time = f"{formatted_date} {formatted_time}"
+
         os.system(f"sudo date -s '{formatted_system_time}'")
+
         return
 
     def passive_listen(self, timeout=15):
         """Listen for incoming data with a specified timeout."""
+
         start_time = time.time()
+
         while True:
             if time.time() - start_time > timeout:
                 return None
+
             received_message = self.ser.readline().decode().strip()
+
             if received_message:
                 return received_message
+
             time.sleep(0.1)  # Short delay to prevent busy waiting
 
     def save_message_to_csv(self, input_str, csv_filename="data/messages.csv"):
         if "#" in input_str and len(input_str.split("#")) == 2:
             address, message = input_str.split("#")
+
             if os.path.exists(csv_filename):
                 df = pd.read_csv(csv_filename, header=0)
+
             else:
                 df = pd.DataFrame(columns=[address])
+
                 df.to_csv(csv_filename, index=False)
+
                 return self.save_message_to_csv(input_str, csv_filename)
+
             if address in df.columns:
                 df.loc[
                     df[address].last_valid_index() + 1
@@ -101,22 +144,33 @@ class Lora:
                     else 0,
                     address,
                 ] = message
+
             else:
                 df[address] = None
+
                 df.loc[0, address] = message
+
             df.to_csv(csv_filename, index=False)
+
         else:
             print("The message format is incorrect. No action taken.")
 
     def send_emergency(self, gps_data):
         """Send an emergency signal with GPS data if connected."""
+
         # connected_bool = self.initialize_LoRaWAN()
+
         if self.connected_bool:
             sent_em_message = f"#EM{gps_data}\r\n".encode()
+
             self.ser.write(sent_em_message)
+
             received_message = self.passive_listen()
+
             # self.terminate_LoRaWAN()
+
             print(received_message)
+
         else:
             print("NOT CONNECTED TO NETWORK, CANNOT SEND DISTRESS CALL")
 
@@ -124,74 +178,120 @@ class Lora:
 
     def request_weather_time(self):
         """Request weather data and system time if connected, then save to CSV."""
+
         # connected_bool = self.initialize_LoRaWAN()
+
         if self.connected_bool:
             self.ser.write(b"#WEATHER\n")
+
             received_message = self.passive_listen()
+
             print(received_message)
+
             # self.terminate_LoRaWAN()
+
             weather_data_start_index = received_message.find("[")
+
             system_time_str = received_message[:weather_data_start_index].strip()
+
+            self.change_system_time(system_time_str)
+
             data = json.loads(received_message[weather_data_start_index:].strip())
+
             df = pd.DataFrame(data)
+
             df["p"] = df["p"] * 100  # change from decimal to percentage
+
             df.rename(
                 columns={"m": "max temp", "n": "min temp", "p": "precipitation"},
                 inplace=True,
             )
+
             current_date = datetime.now()
+
             df["day"] = [
                 (current_date + timedelta(days=i)).strftime("%A")
                 for i in range(len(df))
             ]
+
             cols = ["day"] + [col for col in df if col != "day"]
+
             df = df[cols]
+
             system_time_df = pd.DataFrame([system_time_str], columns=["day"])
+
             df = pd.concat([df, system_time_df], ignore_index=True)
+
             print(df)
+
             csv_file_path = "data/weather_data.csv"
+
             df.to_csv(csv_file_path, index=False, header=True)
+
         else:
             print("FAILED TO CONNECT TO NETWORK. CANNOT UPDATE TIME")
+
             # self.terminate_LoRaWAN()
 
     def send_text(self, address, text):
         # connected_bool = self.initialize_LoRaWAN()
+
         if self.connected_bool:
             sent_message = f"{address}#{text}"
+
             self.ser.write(f"{sent_message}\n".encode())
+
             received_message = self.passive_listen()
+
             # time.sleep(15)  # Replaces time.sleep(15)
+
             self.active_listen()
+
             self.save_message_to_csv(sent_message)
+
             if received_message:
                 self.save_message_to_csv(received_message)
+
         else:
             print("NOT CONNECTED TO NETWORK, CANNOT SEND TEXT")
+
             # self.terminate_LoRaWAN()
 
     def active_listen(self, timeout=5):
         while True:
             self.ser.write(b"#LISTEN\n")
+
             start_time = time.time()
+
             while True:
                 time.sleep(1)  # Replaces time.sleep(1)
+
                 if (time.time() - start_time) > timeout:
                     print("Active listen timeout.")
+
                     # self.terminate_LoRaWAN()
+
                     return
+
                 response = self.ser.readline().decode().strip()
+
                 if response:
                     if response == "#FALSE":
                         print("All messages received.")
 
                         # know when to update page
+
                         self.messageDone = True
+
                         # self.terminate_LoRaWAN()
+
                         return
+
                     else:
-                        # print(response)
+                        print(response)
+
                         self.save_message_to_csv(response)
+
                         break
 
 
@@ -199,12 +299,19 @@ if __name__ == "__main__":
     myLora = Lora()
 
     # Example usage with the queue system
+
     myLora.add_to_queue(myLora.send_emergency, "10.1100")
+
     myLora.add_to_queue(myLora.non_blocking_delay, 3)
+
     myLora.add_to_queue(myLora.send_text, "b69b9d14e5e5b0c0", "hey4!")
+
     myLora.add_to_queue(myLora.non_blocking_delay, 3)
+
     myLora.add_to_queue(myLora.active_listen)
+
     myLora.add_to_queue(myLora.terminate_LoRaWAN)
 
     # Wait for the queue to be processed (optional)
+
     myLora.method_queue.join()
